aae94bd0b3e4aca047350f189275620f8f92570c
Karan Aggarwal <karan.aggarwal789@gmail.com>
2017-10-22 10:36:07 +0000

    Initial Look for Web App included

---
+++ app.js
@@ -0,0 +1,18 @@
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose");
+    
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+app.get("/",function(req,res){
+   res.render("landing"); 
+});
+
+
+
+app.listen(process.env.PORT,process.env.IP,function(){
+    console.log("Server Started");
+});
