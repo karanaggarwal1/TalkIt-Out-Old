efbc5fe3473221101aeb260207da16e2d8c0a10d
Karan Aggarwal <karan.aggarwal789@gmail.com>
2017-12-05 23:35:49 +0530

    Boiler plate code for app.js

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-var express = require("express"),
+const express = require("express"),
     app = express(),
     bodyParser = require("body-parser"),
-    firebase = require("firebase");
+    firebase = require("firebase"),
+    passport = require('passport');
 
 var middleware = require("./middleware");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.set("view engine", "ejs");
 
 app.use(express.static(__dirname + "/public")); // makes the folder available for communication to server and backend
